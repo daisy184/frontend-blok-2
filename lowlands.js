1da06e985d50c8c84bcd758884d0fb69651333f9
daisy184 <58260361+daisy184@users.noreply.github.com>
2020-01-08 09:43:33 +0100

    Add files via upload

---
+++ lowlands.js
@@ -0,0 +1,34 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+
+var heartIcon = document.getElementById('hartje1');
+heartIcon.addEventListener('click', function() {
+  heartIcon.classList.add('liked');
+});
+
+var heartIcon2 = document.getElementById('hartje2');
+heartIcon2.addEventListener('click', function() {
+  heartIcon2.classList.add('liked');
+});
+
+var heartIcon3 = document.getElementById('hartje3');
+heartIcon3.addEventListener('click', function() {
+  heartIcon3.classList.add('liked');
+});
+
+var heartIcon4 = document.getElementById('hartje4');
+heartIcon4.addEventListener('click', function() {
+  heartIcon4.classList.add('liked');
+});
+
+var heartIcon5 = document.getElementById('hartje5');
+heartIcon5.addEventListener('click', function() {
+  heartIcon5.classList.add('liked');
+});
+
+var heartIcon6 = document.getElementById('hartje6');
+heartIcon6.addEventListener('click', function() {
+  heartIcon6.classList.add('liked');
+});
