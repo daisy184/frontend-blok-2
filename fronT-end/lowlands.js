9465ede970ad57f65e3eb14a3ce54b235e64c521
daisy184 <58260361+daisy184@users.noreply.github.com>
2020-01-19 20:47:36 +0100

    Add files via upload

--- fronT-end/lowlands.js
+++ fronT-end/lowlands.js
@@ -2,33 +2,41 @@
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
 
+/* bron: https://developer.mozilla.org/nl/docs/Web/API/Document/getElementById
+bron: https://www.w3schools.com/js/js_htmldom_eventlistener.asp*/
+
+/* Var aangemaakt voor 6 verschillende hartjes: heartIcon t/m heartIcon6
+Elk hartje wordt opgehaald door getElementById 
+addEventListener zorgt ervoor dat als je erop klikt, de function wordt uitgevoerd
+Liked staat als class in de CSS en wordt opgehaald: Animatie wordt uitgevoerd = animatie van 2 sec en rood kleuren van het hartje. */
+
 
 var heartIcon = document.getElementById('hartje1');
-heartIcon.addEventListener('click', function() {
-  heartIcon.classList.add('liked');
+heartIcon.addEventListener('click', function () {
+    heartIcon.classList.add('liked');
 });
 
 var heartIcon2 = document.getElementById('hartje2');
-heartIcon2.addEventListener('click', function() {
-  heartIcon2.classList.add('liked');
+heartIcon2.addEventListener('click', function () {
+    heartIcon2.classList.add('liked');
 });
 
 var heartIcon3 = document.getElementById('hartje3');
-heartIcon3.addEventListener('click', function() {
-  heartIcon3.classList.add('liked');
+heartIcon3.addEventListener('click', function () {
+    heartIcon3.classList.add('liked');
 });
 
 var heartIcon4 = document.getElementById('hartje4');
-heartIcon4.addEventListener('click', function() {
-  heartIcon4.classList.add('liked');
+heartIcon4.addEventListener('click', function () {
+    heartIcon4.classList.add('liked');
 });
 
 var heartIcon5 = document.getElementById('hartje5');
-heartIcon5.addEventListener('click', function() {
-  heartIcon5.classList.add('liked');
+heartIcon5.addEventListener('click', function () {
+    heartIcon5.classList.add('liked');
 });
 
 var heartIcon6 = document.getElementById('hartje6');
-heartIcon6.addEventListener('click', function() {
-  heartIcon6.classList.add('liked');
+heartIcon6.addEventListener('click', function () {
+    heartIcon6.classList.add('liked');
 });
